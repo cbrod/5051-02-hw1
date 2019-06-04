fcbe58b151e1f1d4f1648d2abe270217ff43a5c0
cbrod <broderickcel@seattleu.edu>
2019-06-03 20:38:37 -0700

    Added ReportViewModel LogViewModel Get test case

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -47,5 +47,18 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual(testNum, result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            var result = myTest.LogViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
