1170777621783cf4f9e353eb7ffa363b34826c07
cbrod <broderickcel@seattleu.edu>
2019-06-03 20:30:56 -0700

    Added ReportViewModel NumberOfUsers Get test case

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -18,5 +18,19 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_NumberOfUsers_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            var result = myTest.NumberOfUsers;
+
+            // Assert
+            //new int should default to 0
+            Assert.AreEqual(0, result);
+        }
     }
 }
